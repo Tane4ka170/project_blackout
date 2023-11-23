e8efb6cc412ee4c451c0561bf392b5fd4970e7ea
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2023-11-23 17:55:08 +0200

    fonts and styles

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components';
+import fontBold from '../fonts/Suisse-Intl-Bold.ttf';
+import fontRegular from '../fonts/SuisseIntl-Regular.ttf';
+
+export const GlobalStyles = createGlobalStyle`
+ 
+ @font-face {
+  font-family: 'Suisse Intl';
+  font-weight: 400 600 700;
+  src: url(${fontBold}),url(${fontRegular}) ;
+}
+body{
+font-family: 'Suisse Intl', sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+p:last-child {
+  margin-bottom: 0;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+button {
+  cursor: pointer;
+  border: none;
+  color: inherit;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}`;
