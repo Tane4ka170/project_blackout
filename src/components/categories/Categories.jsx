331b0b6e8359bd455e36d8217b2e3e154bdee73a
Kreal11 <kirillshyrokov2@gmail.com>
2023-11-24 16:31:12 +0200

    added deleted category feature

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,18 +1,28 @@
 import { OneCategory } from 'components/oneCategory/OneCategory';
+import { selectIsLoggedIn } from 'components/redux/auth/selectors';
 import {
   createCategoryThunk,
   deleteCategoryThunk,
+  getCategoriesThunk,
 } from 'components/redux/category/operations';
 import { selectCategories } from 'components/redux/category/selectors';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 
 export const Categories = ({ type = 'incomes' }) => {
   const categories = useSelector(selectCategories);
+  console.log(categories);
   const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   const { register, handleSubmit, reset } = useForm();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(getCategoriesThunk());
+    }
+  }, [dispatch, isLoggedIn]);
 
   const submit = ({ categoryName }) => {
     const categoryDate = { type, categoryName };
@@ -24,15 +34,21 @@ export const Categories = ({ type = 'incomes' }) => {
     <div>
       <p>All categories</p>
       <ul>
-        {categories?.map(category => {
-          return (
-            <OneCategory
-              key={category.id}
-              {...category}
-              deleteCategory={() => dispatch(deleteCategoryThunk(category.id))}
-            />
-          );
-        })}
+        {categories.length ? (
+          categories?.map(category => {
+            return (
+              <OneCategory
+                key={category._id}
+                {...category}
+                deleteCategory={() =>
+                  dispatch(deleteCategoryThunk(category._id))
+                }
+              />
+            );
+          })
+        ) : (
+          <p>There are no categories yet</p>
+        )}
       </ul>
       <form action="" onSubmit={handleSubmit(submit)}>
         <p>New category</p>
--- src/components/oneCategory/OneCategory.jsx
+++ src/components/oneCategory/OneCategory.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export const OneCategory = ({ id, categoryName, deleteCategory }) => {
-  return <li>{categoryName}</li>;
+export const OneCategory = ({ _id, categoryName, deleteCategory }) => {
+  return (
+    <li>
+      {categoryName}
+      <button>Choose</button>
+      <button>Edit</button>
+      <button type="button" onClick={() => deleteCategory(_id)}>
+        Delete
+      </button>
+    </li>
+  );
 };
