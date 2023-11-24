739b9f69e2a2b5e3b9c71aa05e77aa8f7151f3cd
AnnaMartsinovska <annmartsinovska@gmail.com>
2023-11-24 12:38:00 +0200

    База на модальне вікно(open, close)

---
+++ src/components/redux/modal/reducers.js
@@ -0,0 +1,16 @@
+const initialState = {
+  isModalOpen: false,
+};
+
+const modalReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'OPEN_MODAL':
+      return { ...state, isModalOpen: true };
+    case 'CLOSE_MODAL':
+      return { ...state, isModalOpen: false };
+    default:
+      return state;
+  }
+};
+
+export default modalReducer;
