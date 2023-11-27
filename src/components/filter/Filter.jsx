0d9721bcb8c5eecaaefb5e72a42ccb6fefcf64fb
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-11-27 22:31:44 +0200

    update

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -24,8 +24,6 @@ export const Filter = () => {
     setTempFilter(e.target.value);
   };
 
-  console.log(filter);
-
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   const handleDateChange = date => {
