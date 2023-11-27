cf78bcac27e249ca3e894c99c814d6c930ad2f7f
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-11-27 18:49:14 +0200

    update

--- src/pages/Income/Income.jsx
+++ src/pages/Income/Income.jsx
@@ -11,6 +11,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import {
   DIV,
+  DIV375,
   DIVL,
   DIVMAIN,
   DIVTR,
@@ -23,17 +24,22 @@ import {
   P3,
   P4,
   P5,
+  PEr,
   PH,
   TransactionsContainer,
   ULL,
 } from './Income.styled';
 import { TotalExpense, TotalIncome } from 'shared/Total';
+import svg from '../../images/Sprite.svg';
 
 export const Income = () => {
   const dispatch = useDispatch();
   const filter = useSelector(selectFilter);
   const startDate = useSelector(selectStartDate);
   const transactions = useSelector(selectTransaction);
+  const date = useSelector(selectStartDate);
+
+  console.log(date);
 
   useEffect(() => {
     dispatch(getTransactionsThunk({ type: 'incomes', date: startDate }));
@@ -43,24 +49,18 @@ export const Income = () => {
     console.log(transactionId);
     dispatch(
       deleteTransactionThunk({
-        type: 'incomes',
         id: transactionId,
       })
     );
     toast.success(`Transaction ${transactionComment} was deleted`);
   };
 
-  // const filterContacts = transactions.filter(
-  //   transaction =>
-  //     transaction.category.categoryName
-  //       .toLowerCase()
-  //       .trim()
-  //       .includes(filter.toLowerCase().trim()) ||
-  //     transaction.comment
-  //       .toLowerCase()
-  //       .trim()
-  //       .includes(filter.toLowerCase().trim())
-  // );
+  const filterTransactions = transactions.filter(transaction =>
+    transaction.comment
+      .toLowerCase()
+      .trim()
+      .includes(filter.toLowerCase().trim())
+  );
 
   return (
     <div>
@@ -84,26 +84,41 @@ export const Income = () => {
       </DIVMAIN>
       <DIVTR>
         <Filter />
-        <SectionTransaction />
-        <TransactionsContainer>
-          {transactions.map(transaction => (
-            <DIV key={transaction._id}>
-              <P1>{transaction.category.categoryName}</P1>
-              <P2>{transaction.comment}</P2>
-              <P3>{transaction.date}</P3>
-              <P4>{transaction.time}</P4>
-              <P5>{transaction.sum} / UAH</P5>
-              <EditBtn>Edit</EditBtn>
-              <DelBtn
-                onClick={() =>
-                  handleDelete(transaction._id, transaction.comment)
-                }
-              >
-                Delete
-              </DelBtn>
-            </DIV>
-          ))}
-        </TransactionsContainer>
+        <DIV375>
+          <SectionTransaction />
+          {filterTransactions.length ? (
+            <TransactionsContainer>
+              {transactions.map(transaction => (
+                <DIV key={transaction._id}>
+                  <P1>{transaction.category.categoryName}</P1>
+                  <P2>{transaction.comment}</P2>
+                  <P3>{transaction.date}</P3>
+                  <P4>{transaction.time}</P4>
+                  <P5>{transaction.sum} / UAH</P5>
+                  <EditBtn>
+                    <svg width={16} height={16}>
+                      <use href={svg + '#icon-edit-2'}></use>
+                    </svg>
+                    <span>Edit</span>
+                  </EditBtn>
+                  <DelBtn
+                    onClick={() =>
+                      handleDelete(transaction._id, transaction.comment)
+                    }
+                  >
+                    <svg width={16} height={16}>
+                      <use href={svg + '#icon-trash-2'}></use>
+                    </svg>
+                    <span>Delete</span>
+                  </DelBtn>
+                </DIV>
+              ))}
+            </TransactionsContainer>
+          ) : null}
+          {filter.trim().length > 0 && !filterTransactions.length && (
+            <PEr>We couldn't find any transactions matching your request.</PEr>
+          )}
+        </DIV375>
       </DIVTR>
     </div>
   );
