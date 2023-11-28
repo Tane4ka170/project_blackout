10c2a595ca2d629676a6e4b1e036348eba22d57a
overmind777 <63595243+overmind777@users.noreply.github.com>
2023-11-28 02:27:42 +0200

    Update Doughnut.jsx

--- src/components/doughnut/Doughnut.jsx
+++ src/components/doughnut/Doughnut.jsx
@@ -1,4 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import { Doughnut } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
@@ -10,87 +12,84 @@ import {
   MainWrapper,
   Wrapper,
 } from './Doughnut.styled';
+import { selectTransaction } from 'redux/transactions/selectors';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const colors = ['#0EBB69', '#0EF387', '#FAFAFA', 'rgba(250, 250, 250, 0.20)'];
-
-const chartOptions = {
-  hoverOffset: [30],
-  rotation: [-90],
-  circumference: [180],
-  maintainAspectRatio: false,
-  width: [280],
-  height: [140],
-};
-
 const DoughnutComponent = () => {
   const listRef = useRef(null);
-  const [selectedItem, setSelectedItem] = useState(null);
+  const queryTransactions = useSelector(selectTransaction);
+  const [allSums, setAllSums] = useState(null);
+  const [items, setItems] = useState(null);
 
-  const textCenter = {
-    id: 'textCenter',
-    beforeDatasetDraw(chart,
-      args,
-      pluginOptions
-    ) {
-      const {
-        ctx,
-        // data
-      } = chart;
-      const xCenter = chart.getDatasetMeta(0).data[0].x;
-      const yCenter = chart.getDatasetMeta(0).data[0].y;
-
-      ctx.save();
-      ctx.font = '54px';
-      ctx.textAlign = 'center';
-      ctx.fillStyle = '#FAFAFA';
-      ctx.textBaseline = 'bottom';
-
-      const displayText = selectedItem
-        ? `${selectedItem.number}%`
-        : 'Ваш текст';
-      ctx.fillText(`${displayText}%`, xCenter, yCenter);
-    },
+  const colors = [
+    '#007E7E',
+    '#00FF00',
+    '#57FF57',
+    '#008A8A',
+    '#009A9A',
+    '#00C5C5',
+    '#006666',
+    '#00B2B2',
+    '#00DADA',
+    '#005C5C',
+    '#007272',
+    '#00E5E5',
+    '#004040',
+    '#00A5A5',
+    '#00F0F0',
+    '#003434',
+    '#009292',
+    '#00CDCD',
+    '#002E2E',
+    '#00FFFF',
+    '#001919',
+    '#00D5D5',
+    '#006262',
+    '#003D3D',
+    '#00AAAA',
+    '#002323',
+    '#00BFBF',
+    '#00ECEC',
+    '#004747',
+    '#00F5F5',
+    '#000A0A',
+    '#00CACA',
+    '#005353',
+    '#00F2F2',
+    '#008080',
+    '#001E1E',
+    '#00F7F7',
+    '#000505',
+    '#00A9A9',
+    '#009090',
+    '#000000',
+  ];
+  const chartOptions = {
+    hoverOffset: [30],
+    rotation: [-90],
+    circumference: [180],
+    maintainAspectRatio: false,
+    width: [280],
+    height: [140],
   };
 
   const [chartData, setChartData] = useState({
     labels: [],
     datasets: [
       {
-        label: 'Мій перший набір даних',
-        data: [300, 50, 100],
+        label: '',
+        data: [0],
         backgroundColor: colors,
-        borderWidth: 0,
+        borderWidth: 1,
       },
     ],
   });
-
-  const [items, setItems] = useState([
-    {
-      name: 'Хобі1',
-      number: 45,
-      id: 0,
-    },
-    {
-      name: 'Хобі2',
-      number: 45,
-      id: 1,
-    },
-    {
-      name: 'Хобі3',
-      number: 45,
-      id: 2,
-    },
-  ]);
 
   const handleChartHover = elements => {
     if (elements.length > 0) {
       const clickedElement = elements[0];
       const index = clickedElement.index;
-
-      // Оновлюємо вибраний елемент
-      setSelectedItem(items[index]);
 
       if (listRef.current) {
         const listItem = listRef.current.querySelector(`[data-id="${index}"]`);
@@ -106,35 +105,77 @@ const DoughnutComponent = () => {
     }
   };
 
-  const addDataPoint = () => {
-    const newDataPoint = {
-      name: `Хобі ${items.length + 1}`,
-      number: Math.floor(Math.random() * 50) + 1, // Генеруємо випадкове значення в межах 1-50
-      id: items.length,
-    };
+  useEffect(() => {
+    if (queryTransactions) {
+      const categorySums = {};
 
-    const newColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(
-      Math.random() * 256
-    )}, ${Math.floor(Math.random() * 256)}, 0.8)`;
+      // Розрахунок сум для кожної унікальної категорії
+      queryTransactions.forEach(item => {
+        const categoryName = item.category?.categoryName;
 
-    // Оновлюємо стан графіка
-    setChartData(prevData => ({
-      labels: [],
-      datasets: [
-        {
-          data: [...(prevData.datasets[0]?.data || []), newDataPoint.number],
-          backgroundColor: [
-            ...(prevData.datasets[0]?.backgroundColor || []),
-            newColor,
-          ],
-          borderWidth: 0,
-        },
-      ],
-    }));
+        if (categoryName) {
+          if (categorySums[categoryName]) {
+            categorySums[categoryName] += item.sum;
+          } else {
+            categorySums[categoryName] = item.sum;
+          }
+        }
+      });
 
-    // Оновлюємо стан списку
-    setItems(prevItems => [...prevItems, newDataPoint]);
-  };
+      // Загальна сума всіх транзакцій
+      const totalSum = queryTransactions.reduce(
+        (acc, item) => acc + item.sum,
+        0
+      );
+
+      // Створення масиву об'єктів для відображення в графіку та списку
+      const items = Object.keys(categorySums).map(categoryName => ({
+        category: { categoryName },
+        sum: categorySums[categoryName],
+        percentage: ((categorySums[categoryName] / totalSum) * 100).toFixed(2),
+      }));
+
+      setItems(items);
+    }
+  }, [queryTransactions]);
+
+  useEffect(() => {
+    if (items) {
+      const sumByCategory = items.reduce((acc, transaction) => {
+        const categoryName = transaction.category?.categoryName;
+        if (categoryName) {
+          acc[categoryName] = (acc[categoryName] || 0) + transaction.sum;
+        }
+        return acc;
+      }, {});
+
+      const sortedItems = [...items].sort((a, b) =>
+        a.category.categoryName > b.category.categoryName ? 1 : -1
+      );
+
+      const newData = Object.values(sumByCategory);
+
+      setChartData(prevData => ({
+        labels: [],
+        datasets: [
+          {
+            data: newData,
+            backgroundColor: [
+              ...(prevData.datasets[0]?.backgroundColor || []),
+              colors,
+            ],
+            borderWidth: 0,
+          },
+        ],
+      }));
+
+      const totalSum = sortedItems.reduce(
+        (acc, transaction) => acc + transaction.sum,
+        0
+      );
+      setAllSums(totalSum);
+    }
+  }, [items]);
 
   return (
     <Wrapper>
@@ -147,26 +188,22 @@ const DoughnutComponent = () => {
               ...chartOptions,
               onHover: (_, elements) => handleChartHover(elements),
             }}
-            plugins={[textCenter]}
           />
         </DoughnutWrapper>
         <ListWrapper ref={listRef}>
           <ListStyled>
-            {items?.map((item, index) => (
+            {items?.map((transaction, index) => (
               <ItemStyled
                 data-id={index}
-                key={item.id}
+                key={index}
                 color={chartData.datasets[0]?.backgroundColor[index]}
               >
-                <p>{item.name}</p>
-                <p>{item.number}%</p>
+                <p>{transaction.category?.categoryName}</p>
+                <p>{`${((transaction.sum / allSums) * 100).toFixed(2)}%`}</p>
               </ItemStyled>
             ))}
           </ListStyled>
         </ListWrapper>
-        <button data-id={items.length} onClick={addDataPoint}>
-          Додати дані
-        </button>
       </MainWrapper>
     </Wrapper>
   );
