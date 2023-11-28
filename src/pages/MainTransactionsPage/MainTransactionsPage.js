1213405e737578706a690fe8064359aa27310217
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-11-28 21:21:46 +0200

    fix preSelectedType

--- src/pages/MainTransactionsPage/MainTransactionsPage.js
+++ src/pages/MainTransactionsPage/MainTransactionsPage.js
@@ -3,8 +3,8 @@ import React, { useEffect } from "react";
 import { useMediaQuery } from 'react-responsive'
 
 // selectors
-  import { useDispatch, useSelector } from "react-redux";
-  import { selectIsLoggedIn } from "redux/auth/selectors.js";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsLoggedIn } from "redux/auth/selectors.js";
 
 // components
 import { TotalIncome, TotalExpense } from 'shared/Total';
@@ -25,16 +25,18 @@ import {
 // framer animation
 import { motion } from 'framer-motion';
 
-
-import { Navigate } from 'react-router-dom';
+// thunk's
 import { getTransactionsThunk } from "redux/transactions/operations";
 
 
 const MainTransactionsPage = () => {
+  // adaptive design
   const isNotDesktop = useMediaQuery({ query: '(max-width: 1439.98px' })
-  
   const isDesktop = useMediaQuery({ query: '(min-width: 1440px)' })
 
+
+  
+  // dispatch to get all transactions
   const isLoggedIn = useSelector(selectIsLoggedIn)
   const dispatch = useDispatch()
 
@@ -45,11 +47,6 @@ const MainTransactionsPage = () => {
     
   }, [dispatch, isLoggedIn])
 
-
-  if (!isLoggedIn) {
-    return <Navigate to="/login" />;
-  }
-  
   return (
     <StyledSection>
       {isNotDesktop && (
